1783ddd61b89856a404da85cd2d21a663f2f2a2f
Quackmatic <tgalvin97+github@gmail.com>
2014-10-07 11:41:16 +0100

    Recreated files that were Untracked by VS for some stupid reason

---
+++ Graphmatic/Expressions/Tokens/AbsoluteToken.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Graphmatic.Expressions.Tokens
+{
+    public class AbsoluteToken : FunctionToken
+    {
+        public AbsoluteToken(Expression parent)
+            : base(parent, "Abs")
+        {
+        }
+
+        public AbsoluteToken(Expression parent, XElement xml)
+            :base(parent, xml)
+        {
+
+        }
+
+        public override XElement ToXml()
+        {
+            return new XElement("Absolute",
+                new XElement("Operand", Operand.ToXml()));
+        }
+
+        public virtual void Paint(Graphics g, ExpressionCursor expressionCursor, int x, int y)
+        {
+            Operand.Paint(g, expressionCursor, x + 1, y);
+            g.DrawLine(Expression.ExpressionPen,
+                x,
+                y,
+                x,
+                y + Height);
+
+            g.DrawLine(Expression.ExpressionPen,
+                x + Operand.Width + 2,
+                y,
+                x + Operand.Width + 2,
+                y + Height);
+        }
+
+        public virtual void RecalculateDimensions(ExpressionCursor expressionCursor)
+        {
+            Operand.Size = Size;
+            Operand.RecalculateDimensions(expressionCursor);
+            Width = Operand.Width + 3;
+            Height = Operand.Height;
+        }
+    }
+}
---
+++ Graphmatic/Expressions/Tokens/SymbolicToken.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Graphmatic.Expressions.Tokens
+{
+    public class SymbolicToken : SimpleToken
+    {
+        private SymbolicType _Type;
+        public SymbolicType Type{
+            get { return _Type; }
+            set { _Type = value; }
+        }
+
+        public override string Text
+        {
+	        get 
+	        { 
+		        switch (Type){
+                    case SymbolicType.Comma:
+                        return ",";
+                    case SymbolicType.DecimalPoint:
+                        return ".";
+                    case SymbolicType.Exp10:
+                        return "*~";
+                    case SymbolicType.Percent:
+                        return "%";
+                    default:
+                        return "<unknown symbolic>";
+                }
+	        }
+	        protected set 
+	        { 
+		        throw new NotImplementedException(); 
+	        }
+        }
+
+        public SymbolicToken(Expression parent, SymbolicType type)
+            :base(parent)
+        {
+            Type = type;
+        }
+
+        public SymbolicToken(Expression parent, XElement xml)
+            :base(parent)
+        {
+            string symbolicTypeName = xml.Element("Type").Value;
+            if (!Enum.TryParse<SymbolicType>(symbolicTypeName, out _Type))
+                throw new NotImplementedException("The symbolic type " + symbolicTypeName + " is not implemented.");
+        }
+
+        public override XElement ToXml()
+        {
+            return new XElement("Symbolic",
+                new XAttribute("Type", _Type.ToString()));
+        }
+
+        public enum SymbolicType
+        {
+            Comma,
+            Percent,
+            Exp10,
+            DecimalPoint
+        }
+    }
+}
