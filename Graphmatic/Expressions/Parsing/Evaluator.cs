0ac6b209d3f0353496f6c71c9f67dc25b2e27bc5
Quackmatic <tgalvin97+github@gmail.com>
2014-11-14 22:47:36 +0000

    Moving evaluators to the associated generating token classes

--- Graphmatic/Expressions/Parsing/Evaluator.cs
+++ Graphmatic/Expressions/Parsing/Evaluator.cs
@@ -8,9 +8,4 @@ namespace Graphmatic.Expressions.Parsing
 {
     public delegate double UnaryEvaluator(double operand);
     public delegate double BinaryEvaluator(double left, double right);
-
-    public static class Evaluator
-    {
-        public const UnaryEvaluator Absolute = x => Math.Abs(x);
-    }
 }
