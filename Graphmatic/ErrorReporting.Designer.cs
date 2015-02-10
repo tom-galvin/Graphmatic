7cd7aba65a8f416078952848bd17ece99588128f
Quackmatic <tgalvin97+github@gmail.com>
2015-02-10 12:20:31 +0000

    Added error reporting dialog - complete this!

---
+++ Graphmatic/ErrorReporting.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Graphmatic
+{
+    partial class ErrorReporting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ErrorReporting));
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.labelInformation = new System.Windows.Forms.Label();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.buttonSaveReport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.labelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.labelHeader.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHeader.Location = new System.Drawing.Point(0, 0);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(444, 63);
+            this.labelHeader.TabIndex = 0;
+            this.labelHeader.Text = "Header";
+            this.labelHeader.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // labelInformation
+            // 
+            this.labelInformation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelInformation.Location = new System.Drawing.Point(12, 73);
+            this.labelInformation.Name = "labelInformation";
+            this.labelInformation.Size = new System.Drawing.Size(420, 68);
+            this.labelInformation.TabIndex = 1;
+            this.labelInformation.Text = resources.GetString("labelInformation.Text");
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonClose.Location = new System.Drawing.Point(357, 144);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 2;
+            this.buttonClose.Text = "&Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            // 
+            // buttonSaveReport
+            // 
+            this.buttonSaveReport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonSaveReport.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonSaveReport.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSaveReport.Image = global::Graphmatic.Properties.Resources.bug__arrow;
+            this.buttonSaveReport.Location = new System.Drawing.Point(15, 144);
+            this.buttonSaveReport.Name = "buttonSaveReport";
+            this.buttonSaveReport.Size = new System.Drawing.Size(103, 23);
+            this.buttonSaveReport.TabIndex = 3;
+            this.buttonSaveReport.Text = "&Save Report";
+            this.buttonSaveReport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonSaveReport.UseVisualStyleBackColor = true;
+            // 
+            // ErrorReporting
+            // 
+            this.AcceptButton = this.buttonSaveReport;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonClose;
+            this.ClientSize = new System.Drawing.Size(444, 179);
+            this.Controls.Add(this.buttonSaveReport);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.labelInformation);
+            this.Controls.Add(this.labelHeader);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "ErrorReporting";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Error Reporting";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHeader;
+        private System.Windows.Forms.Label labelInformation;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Button buttonSaveReport;
+    }
+}
---
+++ Graphmatic/ErrorReporting.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Graphmatic
+{
+    public partial class ErrorReporting : Form
+    {
+        public ErrorReporting()
+        {
+            InitializeComponent();
+        }
+    }
+}
